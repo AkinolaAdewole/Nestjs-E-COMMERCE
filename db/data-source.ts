29104fdf2c7ce11a2ca3fd819c689f6ae035a33f
Akinola Adewole <adewoleakinola24@gmail.com>
2024-02-19 17:48:57 +0100

    ecommerce nest

--- db/data-source.ts
+++ db/data-source.ts
@@ -6,7 +6,7 @@ export const dataSourceOptions:DataSourceOptions={
     host:process.env.DB_HOST,
     port: process.env.DB_PORT,
     username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
+    password:Number(process.env.DB_PASSWORD),
     database: process.env.DB_DATABASE,
     entities:[],
     migration:[],
