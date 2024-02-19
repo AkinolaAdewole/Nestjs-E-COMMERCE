5ccef583812d0951ea46fc7e492e1c35c560e034
Akinola Adewole <adewoleakinola24@gmail.com>
2024-02-19 17:04:43 +0100

    ecommerce nest

--- db/data-source.ts
+++ db/data-source.ts
@@ -1,5 +1,16 @@
 import { DataSource, DataSourceOptions } from "typeorm";
 
 export const dataSourceOptions:DataSourceOptions={
-    
+    type:'postgres',
+    host:'localhost',
+    port: 5432,
+    username: 'postgres',
+    password: '54321',
+    database: 'ecommerce-nest',
+    entities:[],
+    migration:[],
+    logging:false,
+    synchronize:false
 }
+
+const dataSource = new DataSource(dataSourceOptions);
