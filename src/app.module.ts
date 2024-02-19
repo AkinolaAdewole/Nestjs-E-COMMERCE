9a2c96ebb07ca10bcb4e76f3f4c0b5072ad91ca2
Akinola Adewole <adewoleakinola24@gmail.com>
2024-02-19 19:13:36 +0100

    ecommerce nest

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { dataSourceOptions } from 'db/data-source';
 import { TestModule } from './test/test.module';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(dataSourceOptions), TestModule],
+  imports: [TypeOrmModule.forRoot(dataSourceOptions), TestModule, UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
