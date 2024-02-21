adf384e593040ab74d4644b33bd15160cf250145
Akinola Adewole <adewoleakinola24@gmail.com>
2024-02-21 13:49:04 +0100

    Ecoomerce NEST

--- src/users/dto/user-signup.dto.ts
+++ src/users/dto/user-signup.dto.ts
@@ -1,12 +1,15 @@
-import { isEmail, isNotEmpty, isString } from "class-validator";
+import { MinLength, isEmail, isNotEmpty, isString } from "class-validator";
 
 export class UserSignUp{
     @isNotEmpty({message:'Name is Null'})
     @isString({message:'Name is string'})
     name: string;
 
-    @isNotEmpty({message:'Name is Null'})
-    @isEmail({message:'Name is string'})
+    @isNotEmpty({message:'email is empty'})
+    @isEmail({},{message:'Please, provide a valid email'})
     email:string;
+
+    @isNotEmpty({message:'password is empty'})
+    @MinLength(5,{message:'password minimum character should be 5'})
     password: string;
 }
