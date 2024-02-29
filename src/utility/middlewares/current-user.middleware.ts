066179a8a8b2b22d3a19a722529a1337a52dd70b
Akinola Adewole <adewoleakinola24@gmail.com>
2024-02-29 18:56:56 +0100

    Nest E-cmmmerce

--- src/utility/middlewares/current-user.middleware.ts
+++ src/utility/middlewares/current-user.middleware.ts
@@ -2,8 +2,17 @@ import { Injectable, NestMiddleware, Req } from '@nestjs/common';
 import { isArray } from 'class-validator';
 import { Request, Response, NextFunction } from 'express';
 import { verify } from 'jsonwebtoken';
+import { UserEntity } from 'src/users/entities/user.entity';
 import { UsersService } from 'src/users/users.service';
 
+
+declare global{
+    namespace Express{
+        interface Request{
+            currentUser?:UserEntity
+        }
+    }
+}
 @Injectable()
 export class CurrentUserMiddleware implements NestMiddleware {
     constructor(private readonly usersService: UsersService) { }
@@ -16,16 +25,20 @@ export class CurrentUserMiddleware implements NestMiddleware {
         // Check if the authorization header is missing, is an array, or does not start with 'Bearer '
         if (!authHeader || isArray(authHeader) || !authHeader.startsWith('Bearer ')) {
             // If any of the conditions are met, proceed with the next middleware in the chain
+            req.currentUser = null
             next();
         } else {
             // Extract the token from the authorization header
             const token = authHeader.split(' ')[1];
 
             // Decode the JWT token and extract the user's id from it
-            const { id } = verify(token, process.env.ACCESS_TOKEN_SECRET_KEY);
+            const { id } = <JwtPayload>verify(token, process.env.ACCESS_TOKEN_SECRET_KEY);
 
             // Fetch the user associated with the extracted id from the UsersService
             const currentUser = await this.usersService.findOne(+id);
+            req.currentUser = currentUser
+            console.log(currentUser);
+            
 
             // Pass control to the next middleware in the chain
             next();
