b72f0a58f51a5e2cb1f2a60934ee53dd1c0730d8
Akinola Adewole <adewoleakinola24@gmail.com>
2024-02-29 18:46:05 +0100

    Nest E-cmmmerce

--- src/utility/middlewares/current-user.middleware.ts
+++ src/utility/middlewares/current-user.middleware.ts
@@ -1,13 +1,28 @@
 import { Injectable, NestMiddleware, Req } from '@nestjs/common';
 import { isArray } from 'class-validator';
 import { Request, Response, NextFunction } from 'express';
+import { verify } from 'jsonwebtoken';
+import { UsersService } from 'src/users/users.service';
 
 @Injectable()
 export class CurrentUserMiddleware implements NestMiddleware {
-  use(req: Request, res: Response, next: NextFunction) {
+    constructor(private readonly usersService:UsersService){}
+  async use(req: Request, res: Response, next: NextFunction) {
     const authHeader = req.headers.authorization || req.headers.Authorization 
     // console.log(req);
-    if(!authHeader || isArray(authHeader) || !authHeader.startsWith('Bearer'))
-    next();
+    if(!authHeader || isArray(authHeader) || !authHeader.startsWith('Bearer ')){
+        // req.CurrentUser = null;
+        next()
+    }else{
+        const token = authHeader.split(' '[1] );
+        // console.log(token);
+        const {id} = verify(token, process.env.ACCESS_TOKEN_SECRET_KEY)
+        const currentUser = await this.usersService.findOne(+id)
+        next()   
+    }
   }
+}
+
+interface JwtPayload{
+    id:string;
 }
