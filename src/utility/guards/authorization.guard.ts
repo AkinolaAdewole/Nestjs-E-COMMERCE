7ccb64cc67ff24ea3ea211c0bba73a5f47759ae7
Akinola Adewole <adewoleakinola24@gmail.com>
2024-03-06 15:52:58 +0100

    E-commerce Backend Nest.js

--- src/utility/guards/authorization.guard.ts
+++ src/utility/guards/authorization.guard.ts
@@ -1,9 +1,12 @@
 import { Injectable, CanActivate, ExecutionContext } from "@nestjs/common";
+import { Reflector } from "@nestjs/core";
 import { Observable } from "rxjs";
 
 @Injectable()
 export class AuthorizeGuard implements CanActivate{
+    constructor(private reflector:Reflector){}
     canActivate(context: ExecutionContext): boolean | Promise<boolean> | Observable<boolean> {
-        
+        const allowedRoles = this.reflector.get('allowedRoles',context.getHandler()); 
+        const request = context.switchToHttp().getRequest().       
     }
 }
