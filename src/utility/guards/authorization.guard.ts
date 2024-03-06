91379d71bf1697e6b8f152ed7cddd6746f5bbcfe
Akinola Adewole <adewoleakinola24@gmail.com>
2024-03-06 20:54:58 +0100

    E-commerce Backend Nest.js

--- src/utility/guards/authorization.guard.ts
+++ src/utility/guards/authorization.guard.ts
@@ -2,8 +2,27 @@ import { Injectable, CanActivate, ExecutionContext, UnauthorizedException } from
 import { Reflector } from "@nestjs/core";
 import { Observable } from "rxjs";
 
+// @Injectable()
+// export class AuthorizeGuard implements CanActivate{
+//     constructor(private reflector:Reflector){}
+//     canActivate(context: ExecutionContext): boolean{
+//         const allowedRoles = this.reflector.get<string[]>('allowedRoles',context.getHandler()); 
+//         const request = context.switchToHttp().getRequest();
+//         const result = request?.currentUser?.roles.map((role:string)=>allowedRoles.includes(role)).
+//         find((val:boolean)=>val===true);
+//         if(result){
+//             return true;
+//         }else{
+//             throw new UnauthorizedException('Sorry, You are not authorized');
+//         }
+//     }
+// }
+
 @Injectable()
-export class AuthorizeGuard implements CanActivate{
+export const AuthorizeGuard =()=
+
+
+CanActivate{
     constructor(private reflector:Reflector){}
     canActivate(context: ExecutionContext): boolean{
         const allowedRoles = this.reflector.get<string[]>('allowedRoles',context.getHandler()); 
