c299be3adf257b754bcc568aa83b498876897352
Akinola Adewole <adewoleakinola24@gmail.com>
2024-03-16 23:22:21 +0100

    E-commerce Backend Nest.js

--- src/categories/entities/category.entity.ts
+++ src/categories/entities/category.entity.ts
@@ -1,5 +1,18 @@
-import { Entity } from "typeorm";
+import { Column, CreateDateColumn, Entity, PrimaryGeneratedColumn, Timestamp, UpdateDateColumn } from "typeorm";
 
 
-@Entity({name:'categories'});
-export class CategoryEntity {}
+@Entity({name:'categories'}) 
+export class CategoryEntity {
+    @PrimaryGeneratedColumn()
+    id:number;
+    @Column()
+    title:string;
+    @Column()
+    description:string;
+    @CreateDateColumn()
+    createdAt:Timestamp;
+    @UpdateDateColumn()
+    updatedAt:Timestamp;
+}
+
+// User can create multiple categories
