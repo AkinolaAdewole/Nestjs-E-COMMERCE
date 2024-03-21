35144bf17cc346b17fcc37f591a105f23c2b0287
Akinola Adewole <adewoleakinola24@gmail.com>
2024-03-21 16:30:09 +0100

    commit

--- src/products/dto/create-product.dto.ts
+++ src/products/dto/create-product.dto.ts
@@ -1,1 +1,21 @@
-export class CreateProductDto {}
+import { IsNotEmpty, IsNumber, IsPositive, IsString } from "class-validator";
+
+export class CreateProductDto {
+    @IsNotEmpty({message: 'Title cannot be blank'})
+    @IsString()
+    title:string;
+
+    @IsNotEmpty({message: 'Description cannot be empty'})
+    @IsString()
+    description:string;
+
+    @IsNotEmpty({ message: 'Price cannot be empty'})
+    @IsNumber(
+        {maxDecimalPlaces: 2},
+        {message: 'Price should be number and max decimal precion is 2'}
+    )
+    @IsPositive({ message : 'Price should be positive number'})
+    price:number;
+
+    @IsNotEmpty({ message: 'Stock cannot be empty'})
+}
