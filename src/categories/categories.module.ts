ab3be46ee4cc2ba18a49420ad28b32ff27fdb3bc
Akinola Adewole <adewoleakinola24@gmail.com>
2025-02-17 23:33:56 +0100

    e-commerce continues

--- src/categories/categories.module.ts
+++ src/categories/categories.module.ts
@@ -5,8 +5,13 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { CategoryEntity } from './entities/category.entity';
 
 @Module({
+  // Registering the CategoryEntity with TypeORM so it can be used in database operations
   imports:[TypeOrmModule.forFeature([CategoryEntity])],
+
+   // Register the controller that handles incoming requests related to categories
   controllers: [CategoriesController],
+
+  // Register the service that contains the business logic related to categories
   providers: [CategoriesService],
   exports: [CategoriesService]
 })
